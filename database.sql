56d478b99aa2f10c236c6e0d4e65399dd6f3e614
MathisCastell <148212506+MathisCastell@users.noreply.github.com>
2024-12-09 11:29:08 +0100

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Salle (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Professeur (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Annee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Classe (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    libelle VARCHAR(255) NOT NULL,
+    fk_prof INT NOT NULL,
+    fk_salle INT NOT NULL,
+    FOREIGN KEY (fk_prof) REFERENCES Professeur(id),
+    FOREIGN KEY (fk_salle) REFERENCES Salle(id)
+);
+
+CREATE TABLE Eleve (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    fk_classe INT NOT NULL,
+    fk_annee INT NOT NULL,
+    redouble BOOLEAN NOT NULL,
+    FOREIGN KEY (fk_classe) REFERENCES Classe(id),
+    FOREIGN KEY (fk_annee) REFERENCES Annee(id)
+);
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';
+GRANT ALL PRIVILEGES ON QuaDav.* TO 'admin'@'localhost';
+FLUSH PRIVILEGES;
