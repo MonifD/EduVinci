1a24666ec22f9ddace054d59aca1f4d1d57f3e34
Mohamad <douri_m@etna-alternance.net>
2024-12-10 11:40:45 +0100

    add controllers inscription

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const controllers = require('../Controllers/Controllers');
 
 // Route pour la page d'accueil
 router.get('/', (req, res) => {
     res.render('index', {});
 });
 
-router.post('/inscription', )
+router.post('/inscription', controllers.registerEleve);
 
 module.exports = router;
