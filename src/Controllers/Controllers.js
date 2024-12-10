3169ede574f0b779d6e7de46eb21c3a83823a8c1
Mathis <mathiscastell@gmail.com>
2024-12-10 11:36:27 +0100

    ajout fonction listEleves

--- src/Controllers/Controllers.js
+++ src/Controllers/Controllers.js
@@ -3,3 +3,37 @@ const { sequelize, Salle, Professeur, Annee, Classe, Eleve, Archive } = require(
 exports.registerEleve = async (req, res, next) => {
 
 };
+
+
+exports.listEleves = async (req, res, next) => {
+  try {
+    const eleves = await Eleve.findAll({
+      include: [
+        {
+          model: Classe,
+          attributes: ['libelle'],
+          include: [
+            {
+              model: Professeur,
+              attributes: ['nom', 'prenom'],
+            },
+            {
+              model: Salle,
+              attributes: ['libelle'],
+            },
+          ],
+        },
+        {
+          model: Annee,
+          attributes: ['libelle'],
+        },
+      ],
+      attributes: ['id', 'nom', 'prenom', 'annee_naissance', 'redouble'],
+    });
+
+    res.status(200).json(eleves);
+  } catch (error) {
+    console.error('Error fetching students:', error);
+    res.status(500).json({ error: 'Une erreur est survenue lors de la récupération des élèves.' });
+  }
+};
